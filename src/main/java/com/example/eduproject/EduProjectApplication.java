9f72775146e5f170fe276c791ec6f27f1d1a8764
Nazarkep355 <Qu@antium3228>
2023-03-26 20:06:22 +0300

    Initial commit

---
+++ src/main/java/com/example/eduproject/EduProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.eduproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduProjectApplication.class, args);
+	}
+
+}
